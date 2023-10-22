d2446d03a9b2e9b434ba73c394b7371d4f43d993
Bedirhan Arslanhan <136608720+albertobed@users.noreply.github.com>
2023-10-22 20:46:34 +0300

    add: search desing

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 "use strict";
 const containerSection = document.querySelector(".home-content__main");
-
 async function fetcData() {
   try {
     const response = await fetch("json/data.json");
@@ -9,7 +8,6 @@ async function fetcData() {
     return data;
   } catch {}
 }
-fetcData();
 
 (async function () {
   const data = await fetcData();
@@ -30,30 +28,130 @@ fetcData();
     const createUl = document.createElement("ul");
     createUl.classList.add("home-content__main-content--lists__items");
 
-    let counter = 0;
-    mlCards.forEach(() => {
-      const sectionCard = `
-      <li class="ml-card">
-         <div class="ml-card--img">
-            <img
-               src=${data[i].ml_cards[counter].img}
-               alt="#" />
-         </div>
-         <div class="ml-card--txt">
-            <h5>${data[i].ml_cards[counter].name}</h5>
-            <span>${data[i].ml_cards[counter].des}</span>
-         </div>
-      </li>
-    `;
+    const CreateMlCards = function () {
+      let counter = 0;
+      mlCards.forEach(() => {
+        const sectionCard = `
+            <li class="ml-card">
+               <div class="ml-card--img">
+                  <img
+                     src=${data[i].ml_cards[counter].img}
+                     alt="#" />
+               </div>
+               <div class="ml-card--txt">
+                  <h5>${data[i].ml_cards[counter].name}</h5>
+                  <span>${data[i].ml_cards[counter].des}</span>
+               </div>
+            </li>
+          `;
 
-      createUl.insertAdjacentHTML("beforeend", sectionCard);
-      counter++;
-    });
+        createUl.insertAdjacentHTML("beforeend", sectionCard);
+        counter++;
+      });
+    };
+    CreateMlCards();
 
     const createDiv = document.createElement("div");
     createDiv.classList.add("home-content__main-content--lists");
     createS.appendChild(createDiv);
     createDiv.appendChild(createUl);
-    containerSection.appendChild(createS);
+    // containerSection.appendChild(createS);
+    containerSection.insertAdjacentElement("afterbegin", createS);
   });
 })();
+///////////////////////////////////
+const containerCardSearch = document.querySelector(".search-content__body");
+async function fetcData2() {
+  try {
+    const response = await fetch("json/data-search.json");
+    const r = await response.json();
+    const data = r.cards;
+    return data;
+  } catch {}
+}
+(async function () {
+  const data = await fetcData2();
+
+  const randomNumber = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1 + min));
+  const randomColor = () =>
+    `rgb(${randomNumber(0, 255)},${randomNumber(0, 255)},${randomNumber(
+      0,
+      255
+    )})`;
+
+  data.forEach(function (d, i) {
+    const catCard = document.createElement("div");
+    catCard.classList.add("search-content__body--card");
+    catCard.style.backgroundColor = randomColor();
+
+    const catSpan = document.createElement("span");
+    catSpan.classList.add("search-content__body--card__title");
+    catSpan.textContent = data[i].title;
+    catCard.appendChild(catSpan);
+
+    const catDiv = document.createElement("div");
+    catDiv.classList.add("search-content__body--card__img");
+
+    const catImg = document.createElement("img");
+    catImg.src = data[i].img;
+
+    catDiv.appendChild(catImg);
+
+    catCard.appendChild(catDiv);
+    containerCardSearch.insertAdjacentElement("afterbegin", catCard);
+    console.log(catCard);
+  });
+})();
+///////////////////////////////////
+const resizer = document.querySelector(
+  ".main-content__wrapper__container__resizer"
+);
+const sidebar = document.querySelector(
+  ".main-content__wrapper__container__menubar"
+);
+
+resizer.addEventListener("mousedown", (event) => {
+  document.addEventListener("mousemove", resize, false);
+  document.addEventListener(
+    "mouseup",
+    () => {
+      document.removeEventListener("mousemove", resize, false);
+    },
+    false
+  );
+});
+
+function resize(e) {
+  const size = `${e.x}px`;
+  sidebar.style.flexBasis = size;
+}
+
+sidebar.style.flexBasis = "325px";
+const mainContent = document.querySelector(
+  ".main-content__wrapper__container__contentbar"
+);
+
+/////////////////////////////////////////////
+const homeBtn = document.querySelector(".btnhome");
+const searchBtn = document.querySelector(".btnsearch");
+const containerHome = document.querySelector(".home-content");
+const containerSearch = document.querySelector(".search-content");
+
+homeBtn.addEventListener("click", function () {
+  containerHome.classList.remove("hidden");
+  containerSearch.classList.add("hidden");
+  searchBtn.classList.remove("active");
+  homeBtn.classList.add("active");
+
+  console.log(containerSection);
+});
+
+searchBtn.addEventListener("click", function () {
+  containerHome.classList.add("hidden");
+  containerSearch.classList.remove("hidden");
+  homeBtn.classList.remove("active");
+  searchBtn.classList.add("active");
+});
+
+///////////////////////////////////////////
